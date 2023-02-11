421d7d85906ea1378cbc9d475822b61210043163
suraj <maysuraj05@gmail.com>
2023-02-11 14:09:11 -0600

    first

---
+++ index.js
@@ -0,0 +1,58 @@
+var no_of_drums=document.querySelectorAll(".drum").length;
+for( var i=0;i<no_of_drums;i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+        var element = this.innerHTML;
+        makesound(element);
+        effect(element);
+
+        });
+}
+        
+        document.addEventListener("keydown", function(event) {
+            makesound(event.key);
+            effect(event.key);
+        });
+
+       function makesound(element) { 
+        switch (element) {
+            case 'w':
+                var audio = new Audio('sounds/tom-1.mp3');
+                audio.play();
+                break;
+            case 'a':
+                var audio = new Audio('sounds/tom-2.mp3');
+                audio.play();
+                break;
+            case 's':
+                var audio = new Audio('sounds/tom-3.mp3');
+                audio.play();
+                break;
+            case 'd':
+                var audio = new Audio('sounds/tom-4.mp3');
+                audio.play();
+                break;            
+            case 'j':
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+            case 'k':
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+            case 'l':
+                    var audio = new Audio('sounds/snare.mp3');
+                    audio.play();
+                    break; 
+            default:
+                console.log(element);
+                break;
+        }
+       }
+    
+function effect(key) {
+    document.querySelector("." + key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("." + key).classList.remove("pressed");
+    }, 100);
+}
---
+++ new.js
@@ -0,0 +1,6 @@
+function ShortFilms(likes, comments, views) {
+    this.likes =likes;
+    this.comments = comments;
+    this.views=views;
+}
+var hrv104= new ShortFilms(304,300,560);
